66425cf5a3f251ebdb1d66c7f1b2664e01bbf101
BigetTimote <timotebiget@gmail.com>
2024-11-04 14:26:50 +0000

    1er Commit

---
+++ script.js
@@ -0,0 +1,6 @@
+function learnMore() {
+    window.location.href = 'http://87.106.104.87/home/accueil.html';
+}
+document.addEventListener('DOMContentLoaded', () => {
+    console.log("Le script est chargé et prêt.");
+});
